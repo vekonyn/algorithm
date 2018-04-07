2a8666763c646ab571d95b692a237bb6f0ad4580
vekony <norbert.vekony@gmail.com>
2018-04-07 19:22:46 +0200

    adding unit test

---
+++ sorting/src/test/java/com/vekony/SortTest.java
@@ -0,0 +1,44 @@
+package com.vekony;
+
+import com.vekony.logaritmic.MergeSort;
+import com.vekony.quadratic.BubbleSort;
+import com.vekony.quadratic.InsertionSort;
+import com.vekony.quadratic.SelectionSort;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@RunWith(JUnit4.class)
+public class SortTest {
+    private final int[] sortable = {5, 1, 9, 3, 12, 40, 3, 7, 17};
+    private final int[] expected = {1, 3, 3, 5, 7, 9, 12, 17, 40};
+
+    private Sort sort;
+
+    @Test
+    public void testBubbleSort() {
+        test(new BubbleSort());
+    }
+
+    @Test
+    public void testInsertionSort() {
+        test(new InsertionSort());
+    }
+
+    @Test
+    public void testSelectionSort() {
+        test(new SelectionSort());
+    }
+
+    @Test
+    public void testMergeSort() {
+        test(new MergeSort());
+    }
+
+    private void test(Sort sort) {
+        sort.sort(sortable);
+        assertArrayEquals(expected, sortable);
+    }
+}
